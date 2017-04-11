6624761cc84ad0b2fdb903fc9e95cd50e90fc68e
Grigusky <gregoire.meunier@live.fr>
2017-04-11 19:29:42 +0200

    five

--- app/src/main/java/com/example/antoinelucas/tetris/MainActivity.java
+++ app/src/main/java/com/example/antoinelucas/tetris/MainActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton menuButton = (ImageButton) findViewById(R.id.buttonMenu);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
