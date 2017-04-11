dd2eae78a6b9d93c32df9da2b8684b0b1f952601
Grigusky <gregoire.meunier@live.fr>
2017-04-11 18:55:11 +0200

    second

--- app/src/main/java/com/example/antoinelucas/tetris/GameActivity.java
+++ app/src/main/java/com/example/antoinelucas/tetris/GameActivity.java
@@ -1,28 +1,20 @@
 package com.example.antoinelucas.tetris;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Grigusky on 11/04/2017.
+ */
 
+public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final Button easyButton = (Button) findViewById(R.id.buttonEasy);
-        easyButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_game);
 
     }
 
@@ -51,3 +43,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 }
+
